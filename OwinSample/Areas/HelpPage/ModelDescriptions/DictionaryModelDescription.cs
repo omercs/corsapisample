7dede219540151e4fe59bc20abc56c5dbfcd4729
Omer Cansizoglu <cansizoc@gmail.com>
2015-01-22 14:52:06 -0800

    example cors

---
+++ OwinSample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OwinCors.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OwinSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace OwinCors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
