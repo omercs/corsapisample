9b3a5daa654fdefc30b3dba5dad422ff39b3ef30
Omer Cansizoglu <cansizoc@gmail.com>
2015-01-22 17:15:20 -0800

    update config

--- OwinSample/App_Start/Startup.Auth.cs
+++ OwinSample/App_Start/Startup.Auth.cs
@@ -17,8 +17,8 @@ namespace OwinCors
                 new WindowsAzureActiveDirectoryBearerAuthenticationOptions
                 {
                     Audience = ConfigurationManager.AppSettings["ida:Audience"],
-                    //Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
-                    MetadataAddress = "https://login.windows-ppe.net/52d4b072-9470-49fb-8721-bc3a1c9912a1/federationmetadata/2007-06/federationmetadata.xml"
+                    Tenant = ConfigurationManager.AppSettings["ida:Tenant"]
+                    //MetadataAddress = "https://login.windows-ppe.net/52d4b072-9470-49fb-8721-bc3a1c9912a1/federationmetadata/2007-06/federationmetadata.xml"
                 });
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
         }
